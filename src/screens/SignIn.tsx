4195d46057322e97b7b7165c72237d92510b302b
Eduardo Andrade <edu2andrade@gmail.com>
2022-07-21 20:30:35 +0100

    Enviroment ready to start

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+// Components starts with capital letters
+export const SignIn = () => {
+  return (
+    <View>
+      <Text>Olá Edu!</Text>
+    </View>
+  );
+};
