680892f71dffc693e702f0ab51babdb93d4aa20e
Eduardo Andrade <edu2andrade@gmail.com>
2022-07-22 07:40:34 +0100

    home interface started

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,9 +3,16 @@ import Logo from "../assets/logo_primary.svg";
 import { FontAwesome } from "@expo/vector-icons";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 // Components starts with capital letters
 export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    console.log(email, password);
+  };
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={"32"}>
       <Logo />
@@ -16,15 +23,17 @@ export const SignIn = () => {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<FontAwesome name="envelope" />} ml="4" />}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
         placeholder="Password"
         InputLeftElement={<Icon as={<FontAwesome name="key" />} ml="4" />}
+        onChangeText={setPassword}
         secureTextEntry
       />
 
-      <Button title="Enter" w="full" />
+      <Button title="ENTER" w="full" onPress={handleSignIn} />
     </VStack>
   );
 };
